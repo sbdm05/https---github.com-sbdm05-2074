8fc5daeaef55d61e7680528c77cc7f2ea8d5778d
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-06 17:00:13 +0200

    Fin jour 4 - formulaire

---
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-order',
+  templateUrl: './form-order.component.html',
+  styleUrls: ['./form-order.component.scss']
+})
+export class FormOrderComponent {
+
+}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -6,6 +6,7 @@ import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orde
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormOrderComponent } from './components/form-order/form-order.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     PageListOrdersComponent,
     PageAddOrderComponent,
     PageEditOrderComponent,
+    FormOrderComponent,
   ],
   imports: [CommonModule, OrdersRoutingModule, SharedModule],
   providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }],
